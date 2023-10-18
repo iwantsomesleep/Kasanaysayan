cf5ce0ad5fb97fe8595519f9f653896eaa42c4bf
Mika <emcalimag@gmail.com>
2023-10-18 19:09:00 +0800

    finished start screen, centered text and fixed user name input

--- Kasanaysayan/Program.cs
+++ Kasanaysayan/Program.cs
@@ -12,63 +12,68 @@ namespace Kasanaysayan
         public static Player currentPlayer = new Player();
         static void Main(string[] args)
         {
-            Start(); // starting screen
-            Introduction(); //
+            Start();
+            Foreword();
             ChapterOne();
-            SampleQuizGame();
+            QuizGame();
             test();
         }
         static void Start()
         {
-            Console.WriteLine(@"      _.--._  _.--._
-,-=.-"":;:;:;\':;:;:;""-._
-\\\:;:;:;:;:;\:;:;:;:;:;\
- \\\:;:;:;:;:;\:;:;:;:;:;\
-  \\\:;:;:;:;:;\:;:;:;:;:;\
-   \\\:;:;:;:;:;\:;::;:;:;:\
-    \\\;:;::;:;:;\:;:;:;::;:\
-     \\\;;:;:_:--:\:_:--:_;:;\    
-      \\\_.-""      :      ""-._\
-       \`_..--""""--.;.--""""--.._=>");
-            Console.WriteLine(@"
- __                                                                                            
-[  |  _                                                                                        
- | | / ]  ,--.   .--.   ,--.   _ .--.   ,--.    _   __  .--.   ,--.    _   __  ,--.   _ .--.   
- | '' <  `'_\ : ( (`\] `'_\ : [ `.-. | `'_\ :  [ \ [  ]( (`\] `'_\ :  [ \ [  ]`'_\ : [ `.-. |  
- | |`\ \ // | |, `'.'. // | |, | | | | // | |,  \ '/ /  `'.'. // | |,  \ '/ / // | |, | | | |  
-[__|  \_]\'-;__/[\__) )\'-;__/[___||__]\'-;__/[\_:  /  [\__) )\'-;__/[\_:  /  \'-;__/[___||__] 
-                                               \__.'                  \__.'                    
-");
+            // center book
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + ("_.--._  _.--._".Length / 2)) + "}", "_.--._  _.--._"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@",-=.- "":;:;:;\':;:;:;""-._".Length / 2)) + "}", @",-=.- "":;:;:;\':;:;:;""-._"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"\\\:;:;:;:;:;\:;:;:;:;:;\".Length / 2)) + "}", @"\\\:;:;:;:;:;\:;:;:;:;:;\"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@" \\\:;:;:;:;:;\:;:;:;:;:;\".Length / 2)) + "}", @"\\\:;:;:;:;:;\:;:;:;:;:;\"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"  \\\:;:;:;:;:;\:;:;:;:;:;\".Length / 2)) + "}", @"\\\:;:;:;:;:;\:;:;:;:;:;\"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"   \\\:;:;:;:;:;\:;::;:;:;:\".Length / 2)) + "}", @"\\\:;:;:;:;:;\:;::;:;:;:\"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"    \\\;:;::;:;:;\:;:;:;::;:\".Length / 2)) + "}", @"\\\;:;::;:;:;\:;:;:;::;:\"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"     \\\;;:;:_:--:\:_:--:_;:;\".Length / 2)) + "}", @"\\\;;:;:_:--:\:_:--:_;:;\"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"      \\\_.-""      :      ""-._\".Length / 2)) + "}", @"\\\_.-""      :      ""-._\"));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"       \`_..--""""--.;.--""""--.._=>".Length / 2)) + "}", @"\`_..--""""--.;.--""""--.._=>"));
 
+            // center title
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@" __                                                                                            ".Length / 2)) + "}", @" __                                                                                            "));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"[  |  _                                                                                        ".Length / 2)) + "}", @"[  |  _                                                                                        "));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@" | | / ]  ,--.   .--.   ,--.   _ .--.   ,--.    _   __  .--.   ,--.    _   __  ,--.   _ .--.   ".Length / 2)) + "}", @" | | / ]  ,--.   .--.   ,--.   _ .--.   ,--.    _   __  .--.   ,--.    _   __  ,--.   _ .--.   "));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@" | '' <  `'_\ : ( (`\] `'_\ : [ `.-. | `'_\ :  [ \ [  ]( (`\] `'_\ :  [ \ [  ]`'_\ : [ `.-. |  ".Length / 2)) + "}", @" | '' <  `'_\ : ( (`\] `'_\ : [ `.-. | `'_\ :  [ \ [  ]( (`\] `'_\ :  [ \ [  ]`'_\ : [ `.-. |  "));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@" | |`\ \ // | |, `'.'. // | |, | | | | // | |,  \ '/ /  `'.'. // | |,  \ '/ / // | |, | | | |  ".Length / 2)) + "}", @" | |`\ \ // | |, `'.'. // | |, | | | | // | |,  \ '/ /  `'.'. // | |,  \ '/ / // | |, | | | |  "));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"[__|  \_]\'-;__/[\__) )\'-;__/[___||__]\'-;__/[\_:  /  [\__) )\'-;__/[\_:  /  \'-;__/[___||__] ".Length / 2)) + "}", @"[__|  \_]\'-;__/[\__) )\'-;__/[___||__]\'-;__/[\_:  /  [\__) )\'-;__/[\_:  /  \'-;__/[___||__] "));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (@"                                               \__.'                  \__.'                    ".Length / 2)) + "}", @"                                               \__.'                  \__.'                    "));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n");
+
+            // press any key to start 
             string pressAnyKey = "PRESS ANY KEY TO START.";
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (pressAnyKey.Length / 2)) + "}", pressAnyKey));
             Console.ForegroundColor = ConsoleColor.White;
             Console.ReadKey(true);
-            Thread.Sleep(1500);
+            Thread.Sleep(500);
             Console.Clear();
+
+            Console.Write("This is the story of... ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            currentPlayer.name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(currentPlayer.name);
         }
 
-        static void Introduction()
+        static void Foreword()
         {
-            string test = "Once upon a time...";
-            Console.WriteLine(test);
-            Console.WriteLine("What is your name, dear traveler?");
-            currentPlayer.name = Console.ReadLine();
-            Console.Clear();
+            Console.WriteLine("hi!");
         }
         static void ChapterOne()
         {
             string chapterOneTitle = "CHAPTER ONE: MAJOHA";
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (chapterOneTitle.Length / 2)) + "}", chapterOneTitle));
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("You see yourself ");
-
-            Console.WriteLine(currentPlayer.name);
+            Console.WriteLine("You open your eyes to a colosseum-like scene, wherein ");
         }
 
-        static void SampleQuizGame()
+        static void QuizGame()
         {
             Console.WriteLine("A history book appears in front of you, with blank pages for you to seemingly fill in.");
 
@@ -85,7 +90,7 @@ namespace Kasanaysayan
                 {
                     Console.WriteLine("You have received 3 revisionism points. You must restart the chapter.");
                     currentPlayer.revisionismPoints = 0;
-                    SampleQuizGame();
+                    QuizGame();
                 }
                 else if (userAnswers == correctAnswers[0])
                 {
